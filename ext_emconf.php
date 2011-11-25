123285efeb07463e7f3407f776e3bf0673842741
ssp <porst@sub.uni-goettingen.de>
2011-11-25 16:29:33 +0100

    1.2.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "nkwgok".
 #
-# Auto generated 24-11-2011 18:37
+# Auto generated 25-11-2011 16:28
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'suggests' => 'ke_stats',
 	'priority' => '',
 	'module' => '',
-	'state' => 'beta',
+	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => 'fileadmin/gok,fileadmin/gok/xml,fileadmin/gok/hitcounts,fileadmin/gok/csv,',
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Göttingen State and University Library, Germany http://www.sub.uni-goettingen.de',
-	'version' => '1.2.0',
+	'version' => '1.2.1',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'ke_stats' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:29:{s:9:"ChangeLog";s:4:"9ac8";s:15:"README.markdown";s:4:"2e32";s:16:"ext_autoload.php";s:4:"ea75";s:21:"ext_conf_template.txt";s:4:"059d";s:12:"ext_icon.gif";s:4:"c8ca";s:17:"ext_localconf.php";s:4:"696c";s:14:"ext_tables.php";s:4:"e7e1";s:14:"ext_tables.sql";s:4:"7ad0";s:23:"icon_tx_nkwgok_data.gif";s:4:"614f";s:13:"locallang.xml";s:4:"49d8";s:16:"locallang_db.xml";s:4:"9ece";s:12:"t3jquery.txt";s:4:"1bfe";s:7:"tca.php";s:4:"0fcc";s:23:"lib/class.tx_nkwgok.php";s:4:"914d";s:26:"lib/class.tx_nkwgok_ff.php";s:4:"2c7b";s:11:"lib/get.php";s:4:"5cd5";s:27:"pi1/class.tx_nkwgok_pi1.php";s:4:"1af8";s:16:"pi1/flexform.xml";s:4:"07f8";s:17:"pi1/locallang.xml";s:4:"ec68";s:14:"res/nkwgok.css";s:4:"0c43";s:41:"scheduler/class.tx_nkwgok_checknewcsv.php";s:4:"fe87";s:40:"scheduler/class.tx_nkwgok_convertcsv.php";s:4:"7557";s:39:"scheduler/class.tx_nkwgok_importall.php";s:4:"8aef";s:42:"scheduler/class.tx_nkwgok_loadfromopac.php";s:4:"9bc4";s:37:"scheduler/class.tx_nkwgok_loadxml.php";s:4:"6ec4";s:70:"scheduler/class.tx_nkwgok_scheduler_convertcsvadditionalparameters.php";s:4:"343b";s:39:"scheduler/class.tx_nkwgok_updatecsv.php";s:4:"12b2";s:23:"scheduler/locallang.xml";s:4:"2d37";s:16:"static/setup.txt";s:4:"2ac2";}',
+	'_md5_values_when_last_written' => 'a:29:{s:9:"ChangeLog";s:4:"b94f";s:15:"README.markdown";s:4:"4308";s:16:"ext_autoload.php";s:4:"ea75";s:21:"ext_conf_template.txt";s:4:"7399";s:12:"ext_icon.gif";s:4:"c8ca";s:17:"ext_localconf.php";s:4:"696c";s:14:"ext_tables.php";s:4:"e7e1";s:14:"ext_tables.sql";s:4:"7ad0";s:23:"icon_tx_nkwgok_data.gif";s:4:"614f";s:13:"locallang.xml";s:4:"49d8";s:16:"locallang_db.xml";s:4:"9ece";s:12:"t3jquery.txt";s:4:"1bfe";s:7:"tca.php";s:4:"0fcc";s:23:"lib/class.tx_nkwgok.php";s:4:"d203";s:26:"lib/class.tx_nkwgok_ff.php";s:4:"2c7b";s:11:"lib/get.php";s:4:"5cd5";s:27:"pi1/class.tx_nkwgok_pi1.php";s:4:"1af8";s:16:"pi1/flexform.xml";s:4:"07f8";s:17:"pi1/locallang.xml";s:4:"ec68";s:14:"res/nkwgok.css";s:4:"0c43";s:41:"scheduler/class.tx_nkwgok_checknewcsv.php";s:4:"fe87";s:40:"scheduler/class.tx_nkwgok_convertcsv.php";s:4:"7557";s:39:"scheduler/class.tx_nkwgok_importall.php";s:4:"8aef";s:42:"scheduler/class.tx_nkwgok_loadfromopac.php";s:4:"38a7";s:37:"scheduler/class.tx_nkwgok_loadxml.php";s:4:"6ec4";s:70:"scheduler/class.tx_nkwgok_scheduler_convertcsvadditionalparameters.php";s:4:"343b";s:39:"scheduler/class.tx_nkwgok_updatecsv.php";s:4:"12b2";s:23:"scheduler/locallang.xml";s:4:"2d37";s:16:"static/setup.txt";s:4:"2ac2";}',
 );
 
 ?>
