cb671a049f43786ef9fb2ea28c833258cdc7068d
ssp <porst@sub.uni-goettingen.de>
2012-02-02 10:34:38 +0100

    version changes

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "nkwgok".
 #
-# Auto generated 30-01-2012 10:53
+# Auto generated 02-02-2012 10:34
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'typo3' => '4.6.4',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:29:{s:9:"ChangeLog";s:4:"91ad";s:15:"README.markdown";s:4:"1d53";s:16:"ext_autoload.php";s:4:"ea75";s:21:"ext_conf_template.txt";s:4:"7399";s:12:"ext_icon.gif";s:4:"c8ca";s:17:"ext_localconf.php";s:4:"696c";s:14:"ext_tables.php";s:4:"e7e1";s:14:"ext_tables.sql";s:4:"7ad0";s:23:"icon_tx_nkwgok_data.gif";s:4:"614f";s:13:"locallang.xml";s:4:"e552";s:16:"locallang_db.xml";s:4:"bca4";s:12:"t3jquery.txt";s:4:"1bfe";s:7:"tca.php";s:4:"0fcc";s:23:"lib/class.tx_nkwgok.php";s:4:"ac88";s:26:"lib/class.tx_nkwgok_ff.php";s:4:"2c7b";s:11:"lib/get.php";s:4:"2d32";s:27:"pi1/class.tx_nkwgok_pi1.php";s:4:"66fd";s:16:"pi1/flexform.xml";s:4:"07f8";s:17:"pi1/locallang.xml";s:4:"205e";s:14:"res/nkwgok.css";s:4:"4c75";s:41:"scheduler/class.tx_nkwgok_checknewcsv.php";s:4:"fe87";s:40:"scheduler/class.tx_nkwgok_convertcsv.php";s:4:"3c34";s:39:"scheduler/class.tx_nkwgok_importall.php";s:4:"8aef";s:42:"scheduler/class.tx_nkwgok_loadfromopac.php";s:4:"777c";s:37:"scheduler/class.tx_nkwgok_loadxml.php";s:4:"d9a1";s:70:"scheduler/class.tx_nkwgok_scheduler_convertcsvadditionalparameters.php";s:4:"343b";s:39:"scheduler/class.tx_nkwgok_updatecsv.php";s:4:"12b2";s:23:"scheduler/locallang.xml";s:4:"2d37";s:16:"static/setup.txt";s:4:"2ac2";}',
+	'_md5_values_when_last_written' => 'a:29:{s:9:"ChangeLog";s:4:"afd9";s:15:"README.markdown";s:4:"1d53";s:16:"ext_autoload.php";s:4:"ea75";s:21:"ext_conf_template.txt";s:4:"7399";s:12:"ext_icon.gif";s:4:"c8ca";s:17:"ext_localconf.php";s:4:"696c";s:14:"ext_tables.php";s:4:"e7e1";s:14:"ext_tables.sql";s:4:"7ad0";s:23:"icon_tx_nkwgok_data.gif";s:4:"614f";s:13:"locallang.xml";s:4:"e552";s:16:"locallang_db.xml";s:4:"bca4";s:12:"t3jquery.txt";s:4:"1bfe";s:7:"tca.php";s:4:"0fcc";s:23:"lib/class.tx_nkwgok.php";s:4:"ac88";s:26:"lib/class.tx_nkwgok_ff.php";s:4:"2c7b";s:11:"lib/get.php";s:4:"2d32";s:27:"pi1/class.tx_nkwgok_pi1.php";s:4:"66fd";s:16:"pi1/flexform.xml";s:4:"07f8";s:17:"pi1/locallang.xml";s:4:"205e";s:14:"res/nkwgok.css";s:4:"4c75";s:41:"scheduler/class.tx_nkwgok_checknewcsv.php";s:4:"fe87";s:40:"scheduler/class.tx_nkwgok_convertcsv.php";s:4:"90dd";s:39:"scheduler/class.tx_nkwgok_importall.php";s:4:"8aef";s:42:"scheduler/class.tx_nkwgok_loadfromopac.php";s:4:"777c";s:37:"scheduler/class.tx_nkwgok_loadxml.php";s:4:"d72d";s:70:"scheduler/class.tx_nkwgok_scheduler_convertcsvadditionalparameters.php";s:4:"343b";s:39:"scheduler/class.tx_nkwgok_updatecsv.php";s:4:"12b2";s:23:"scheduler/locallang.xml";s:4:"2d37";s:16:"static/setup.txt";s:4:"2ac2";}',
 );
 
 ?>
