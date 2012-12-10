5a71fbc86119217f3eca77b7ce5daf6d42e21055
Sven-S. Porst <porst@sub.uni-goettingen.de>
2012-12-10 17:35:57 +0100

    explicitly load constants in FlexForm class
    autoloading does not seem to work there

--- lib/class.tx_nkwgok_ff.php
+++ lib/class.tx_nkwgok_ff.php
@@ -22,6 +22,9 @@
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
+
+require_once t3lib_extMgm::extPath('nkwgok') . 'lib/class.tx_nkwgok_utility.php';
+
 /**
  * Description 
  */
