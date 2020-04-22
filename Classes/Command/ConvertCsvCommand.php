24c5e14ec93a7759fabfcb31337f8a2f0b4c36ae
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2020-04-22 13:45:14 +0200

    Fix Command Return Types as Integer

--- Classes/Command/ConvertCsvCommand.php
+++ Classes/Command/ConvertCsvCommand.php
@@ -26,12 +26,12 @@ class ConvertCsvCommand extends Command
     /**
      * Function executed from the Scheduler.
      *
-     * @return bool TRUE if success, otherwise FALSE
+     * @return int
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $service = GeneralUtility::makeInstance(ConvertCsv::class);
 
-        return $service->run();
+        return $service->run() ? 0 : 1;
     }
 }
